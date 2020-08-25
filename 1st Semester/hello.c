042b6b83945ba322b0ccef06a6f9a7448155ae97
sarthak2806 <sathak2806singhania@gmail.com>
2020-08-25 21:48:04 +0530

    m

--- 1st Semester/hello.c
+++ 1st Semester/hello.c
@@ -1,4 +1,21 @@
 #include <stdio.h>
-int main(void) {
-  printf("Hello, world\n");
+int main()
+{
+  int i;
+  for(i=1;i<=100;i++)
+  {
+    int x;
+    for(x=2;x<i;x++)
+    {
+      if(i%x==0)
+      {
+        break;
+      }
+    }
+    else for
+    {
+      printf("%d is a prime number\n",i);
+    }
+
+  }
 }
