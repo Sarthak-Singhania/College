ac9112afdba2b08b8290dab800b17d5f8ababeee
sarthak2806 <sathak2806singhania@gmail.com>
2020-09-21 18:07:50 +0530

    new

--- 1st Semester/Labs 19-09-2020/C1.c
+++ 1st Semester/Labs 19-09-2020/C1.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
 int main(void)
 {
-    char word[100];
+    char word[100],ch;
+    int score=0;
     printf("Enter a word: ");
     scanf("%s",word);
     if(strcmp(word,"QUIZ")==0 || strcmp(word,"quiz")==0) printf("%d\n",22);
@@ -12,9 +12,19 @@ int main(void)
     {
         for(int i=0;word[i]!='\0';i++)
         {
-            
+            ch=toupper(word[i]);
+            switch (ch)
+            {
+            case 'A': case 'E': case 'I': case 'O': case 'U': case 'N': case 'T': case 'L': case 'R': case 'S': score+=1; break;
+            case 'D': case 'G': score+=2; break;
+            case 'B': case 'C': case 'M': case 'P': score+=3; break;
+            case 'F': case 'H': case 'V': case 'W': case 'Y': score+=4; break;
+            case 'K': score+=5; break;
+            case 'J': case 'X': score+=8; break;
+            case 'Q': case 'Z': score+=10; break;
+            }
         }
     }
-    printf("\n");
+    printf("%d\n",score);
     return 0;
 }
--- 1st Semester/Labs 19-09-2020/C2.c
+++ 1st Semester/Labs 19-09-2020/C2.c
@@ -1,25 +1,46 @@
-#include<stdio.h> 
-int main()
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#include<stdlib.h>
+int main(void)
 {
-	char message[100], ch;
-	int shift;
+    int i,shift,j=0;
+    char message[100],ch;
 	printf("Enter a message to decrypt: ");
 	fgets(message,100,stdin);
 	printf("Enter shift: ");
 	scanf("%d", &shift);
-	for(int i=0; message[i]!='\0'; i++)
+    for(j=0;message[j]!='\0';j++)
     {
-        ch=message[i];
-        if(ch!=' ' && )
+        message[j]=toupper(message[j]);
+    }
+    if(shift>26)
+    {
+        printf("INVALID SHIFT VALUE");
+        exit(0);
+    }
+    for(i=0;i<message[i]!='\0';i++)
+    {
+        if(message[i]==' ')
         {
-            ch-=shift;
-            if(ch<'A')
-            {
-                ch=ch+'Z'-'A'+1;
-            }
-            message[i]=ch;
+        continue;
         }
-	}
-	printf("Decrypted message: %s\n", message);
-	return 0;
+        if(message[i]-shift<65) message[i]=message[i]+26;
+        message[i]-=shift;
+    }
+    for(i=0;i<message[i]!='\0';i++)
+    {
+        if(message[i]==' ')
+        {
+            continue;
+        }
+        if(message[i]=='Q'&&message[i+1]=='Q')
+        {    
+            printf(" ");
+            i+=2;
+        }
+
+        printf("%c",message[i]);
+    }
+    return 0;
 }
