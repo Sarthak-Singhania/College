755de8787752f3a89cca515ab1890501bb060494
sarthak2806 <sarthak.singhania28@gmail.com>
2020-10-17 10:44:58 +0530

    lol

--- 1st Semester/Labs 18-10-2020/C1.c
+++ 1st Semester/Labs 18-10-2020/C1.c
@@ -1,1 +1,45 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int power(int m,int n,int cnt[])
+{ 
+    if(n==0)
+    {
+        return 1;
+    }
+    else if(n%2!=0)
+    {
+        cnt[0]++;
+        return(m*power(m,n-1,cnt));
+    }
+    else
+    {
+        cnt[0]++;
+        return(pow(m,n/2)*power(m,n/2,cnt));
+    }
+
+}
+
+int main(void)
+{
+    int m,n,cnt[]={0};
+    unsigned long long int result=1;
+    printf("Enter base: ");
+    scanf("%d",&m);
+    printf("Enter power: ");
+    scanf("%d",&n);
+    if (m==0 && n==0) printf("Illegal input");
+    if (m==0) 
+    {
+        printf("0, 0\n");
+        exit(0);
+    }
+    if (n%2!=0) result=m*power(m,n-1,cnt);
+    else if (n%2==0)
+    {
+        result=power(m,n/2,cnt);
+        result*=result;
+    }
+    printf("%llu, %d\n",result,cnt[0]);
+    return 0;
+}
--- 1st Semester/Labs 18-10-2020/C3.c
+++ 1st Semester/Labs 18-10-2020/C3.c
@@ -1,25 +1,22 @@
 #include <stdio.h> 
 #include <string.h>
-#include <ctype.h>
-int isPalRec(char str[],int s,int e) 
-{ 
-	if (s==e) return 1; 
-	if (strcasecmp(str[s],str[e])!=0) return 0; 
-	if (s<e+1) return isPalRec(str,s++,e--); 
-	return 1; 
-} 
-
-int isPalindrome(char str[]) 
-{ 
-	int n=strlen(str); 
-	if(n==0) return 1; 
-	return isPalRec(str, 0, n - 1); 
+char isPalRec(char str[],int s, int e) 
+{
+	if (str[s]!=' ' && str[s]!=',')
+	{
+		if (s==e) return 1;
+		if (tolower(str[s])!=tolower(str[e])) return 0;
+		if (s < e + 1) return isPalRec(str, s + 1, e - 1); 
+		return 1; 
+	}
 }
 int main() 
 { 
 	char str[500];
-	scanf("%[^\n]s",str); 
-	if (isPalindrome(str)) printf("Yes\n"); 
+	printf("Enter sentence to check for pallindrome: ");
+	scanf("%[^\n]s",str);
+	int n=strlen(str);
+	if (isPalRec(str, 0, n - 1))  printf("Yes\n"); 
 	else printf("No\n"); 
 	return 0; 
 } 
