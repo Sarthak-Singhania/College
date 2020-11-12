34d98588950a6759c2dcf913230cabffa0229522
sarthak2806 <sathak2806singhania@gmail.com>
2020-11-12 22:00:54 +0530

    new

--- 1st Semester/Labs 15-11-2020/C1 a.c
+++ 1st Semester/Labs 15-11-2020/C1 a.c
@@ -1,34 +1,24 @@
-// Iterative C program to reverse a linked list
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Link list node */
 struct Node {
 	int data;
 	struct Node* next;
 };
 
-/* Function to reverse the linked list */
 static void reverse(struct Node** head_ref)
 {
 	struct Node* prev = NULL;
 	struct Node* current = *head_ref;
 	struct Node* next = NULL;
 	while (current != NULL) {
-		// Store next
 		next = current->next;
-
-		// Reverse current node's pointer
 		current->next = prev;
-
-		// Move pointers one position ahead.
 		prev = current;
 		current = next;
 	}
 	*head_ref = prev;
 }
-
-/* Function to push a node */
 void push(struct Node** head_ref, int new_data)
 {
 	struct Node* new_node
@@ -37,8 +27,6 @@ void push(struct Node** head_ref, int new_data)
 	new_node->next = (*head_ref);
 	(*head_ref) = new_node;
 }
-
-/* Function to print linked list */
 void printList(struct Node* head)
 {
 	struct Node* temp = head;
@@ -48,17 +36,13 @@ void printList(struct Node* head)
 	}
 }
 
-/* Driver code*/
 int main()
 {
-	/* Start with the empty list */
 	struct Node* head = NULL;
-
 	push(&head, 20);
 	push(&head, 4);
 	push(&head, 15);
 	push(&head, 85);
-
 	printf("Given linked list\n");
 	printList(head);
 	reverse(&head);
