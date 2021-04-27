ccb8034466a96617124f40d7a2eb48ce1d8dde0d
Sarthak-Singhania <sarthak.singhania28@gmail.com>
2021-04-27 19:39:24 +0530

    new

--- 2nd Semester/Binary Tree.c
+++ 2nd Semester/Binary Tree.c
@@ -1,36 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
-
 struct bin_tree {
-int data;
-struct bin_tree * right, * left;
+    int data;
+    struct bin_tree *right,*left;
 };
 typedef struct bin_tree node;
 
-void insert(node ** tree, int val)
+void insert(node **tree,int val)
 {
-    node *temp = NULL;
+    node *temp=NULL;
     if(!(*tree))
     {
-        temp = (node *)malloc(sizeof(node));
-        temp->left = temp->right = NULL;
-        temp->data = val;
-        *tree = temp;
+        temp=(node *)malloc(sizeof(node));
+        temp->left=temp->right=NULL;
+        temp->data=val;
+        *tree=temp;
         return;
     }
 
-    if(val < (*tree)->data)
-    {
-        insert(&(*tree)->left, val);
-    }
-    else if(val > (*tree)->data)
-    {
-        insert(&(*tree)->right, val);
-    }
-
+    if(val < (*tree)->data) insert(&(*tree)->left,val);
+    else if(val > (*tree)->data) insert(&(*tree)->right,val);
 }
 
-void print_preorder(node * tree)
+void print_preorder(node *tree)
 {
     if (tree)
     {
@@ -38,10 +30,9 @@ void print_preorder(node * tree)
         print_preorder(tree->left);
         print_preorder(tree->right);
     }
-
 }
 
-void print_inorder(node * tree)
+void print_inorder(node *tree)
 {
     if (tree)
     {
@@ -51,7 +42,7 @@ void print_inorder(node * tree)
     }
 }
 
-void print_postorder(node * tree)
+void print_postorder(node *tree)
 {
     if (tree)
     {
@@ -61,7 +52,7 @@ void print_postorder(node * tree)
     }
 }
 
-void deltree(node * tree)
+void deltree(node *tree)
 {
     if (tree)
     {
@@ -71,64 +62,37 @@ void deltree(node * tree)
     }
 }
 
-node* search(node ** tree, int val)
+node* search(node **tree,int val)
 {
-    if(!(*tree))
-    {
-        return NULL;
-    }
-
-    if(val < (*tree)->data)
-    {
-        search(&((*tree)->left), val);
-    }
-    else if(val > (*tree)->data)
-    {
-        search(&((*tree)->right), val);
-    }
-    else if(val == (*tree)->data)
-    {
-        return *tree;
-    }
+    if(!(*tree)) return NULL;
+    if(val < (*tree)->data) search(&((*tree)->left),val);
+    else if(val > (*tree)->data) search(&((*tree)->right),val);
+    else if(val == (*tree)->data) return *tree;
 }
 
 void main()
 {
     node *root;
     node *tmp;
-    //int i;
-
-    root = NULL;
-    /* Inserting nodes into tree */
-    insert(&root, 9);
-    insert(&root, 4);
-    insert(&root, 15);
-    insert(&root, 6);
-    insert(&root, 12);
-    insert(&root, 17);
-    insert(&root, 2);
-
-    /* Printing nodes of tree */
+    int cont=1,data,search_data;
+    root=NULL;
+    while(cont){
+        printf("Enter data: ");
+        scanf("%d",&data);
+        insert(&root,data);
+        printf("Do you want to continue: ");
+        scanf("%d",&cont);
+    }
     printf("Pre Order Display\n");
     print_preorder(root);
-
     printf("In Order Display\n");
     print_inorder(root);
-
     printf("Post Order Display\n");
     print_postorder(root);
-
-    /* Search node into tree */
-    tmp = search(&root, 4);
-    if (tmp)
-    {
-        printf("Searched node=%d\n", tmp->data);
-    }
-    else
-    {
-        printf("Data Not found in tree.\n");
-    }
-
-    /* Deleting all nodes of tree */
+    printf("Where do you want to search: ");
+    scanf("%d",&search_data);
+    tmp=search(&root,search_data);
+    if (tmp) printf("Searched node=%d\n",tmp->data);
+    else printf("Data Not found in tree.\n");
     deltree(root);
 }
